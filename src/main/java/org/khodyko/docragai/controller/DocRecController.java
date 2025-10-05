adf9e53573f8e4867a5d424c50c62b219d7f7c2e
khodyko <gaes1k@yandex.ru>
2025-10-05 19:09:45 +0300

    add servlet

---
+++ src/main/java/org/khodyko/docragai/controller/DocRecController.java
@@ -0,0 +1,7 @@
+package org.khodyko.docragai.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DocRecController {
+}
---
+++ src/main/java/org/khodyko/docragai/service/impl/DocumentServiceImpl.java
@@ -0,0 +1,56 @@
+package org.khodyko.docragai.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.khodyko.docragai.service.DocumentService;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DocumentServiceImpl implements DocumentService {
+
+    private final VectorStore vectorStore;
+
+    @Override
+    public void readDocumentsFromFolder() {
+        try {
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            Resource[] resources = resolver.getResources("classpath:articles/*");
+            for (Resource resource : resources) {
+                TikaDocumentReader reader = new TikaDocumentReader(resource);
+                TextSplitter textSplitter = new TokenTextSplitter();
+                List<Document> documents = reader.get();
+                for (Document doc:documents){
+                    doc.getMetadata().put("fileName", resource.getFile().getName());
+                }
+                vectorStore.accept(textSplitter.apply(documents));
+            }
+
+        } catch (Exception e) {
+            log.warn("error while read documents",e);
+        }
+        log.debug("allDocS Readed");
+    }
+
+    @Override
+    public void cleanAllDocuments() {
+        List<Document> docs = vectorStore.similaritySearch("");
+        while (!docs.isEmpty()) {
+            for (Document doc : docs) {
+                vectorStore.delete("id == '" + doc.getId() + "'");
+            }
+            docs = vectorStore.similaritySearch("");
+        }
+        log.debug("everything deleted");
+    }
+}
